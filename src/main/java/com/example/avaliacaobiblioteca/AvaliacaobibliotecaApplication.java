9f73b6622658fde3bf2aec92ad7c93d48348686d
Felipe Martins <felipemrtns6002199@gmail.com>
2025-03-13 13:51:08 -0300

    Add files via upload

---
+++ src/main/java/com/example/avaliacaobiblioteca/AvaliacaobibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.example.avaliacaobiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaobibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaobibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/avaliacaobiblioteca/AvaliacaobibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.avaliacaobiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaobibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
